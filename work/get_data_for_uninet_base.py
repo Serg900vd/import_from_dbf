fbc2881173a465a4cc63b8be7b566a5cf7067446
Serg900vd <serg.vd.900@gmail.com>
2022-04-23 15:53:16 +0300

    Добавил в функцию get_firm() параметр filter_id_firm, начал разработку функции get_goods()

--- work/get_data_for_uninet_base.py
+++ work/get_data_for_uninet_base.py
@@ -1,13 +1,17 @@
 # Обновление данных для UNINET base.xls
 #
+from typing import Tuple
+
 PATH_BASE = "d:\\Kotik\\work\\2012_toner_base\\"
+CODEPAGE = 'cp1251'
+FILTR_FIRM_UNINET_HB =(150, 183)
 
 import dbf
 
 
-def get_warehous(PATH_BASE)->list:
+def get_warehous() -> list:
     warehous = []
-    f = dbf.Table(PATH_BASE + 'warehous.dbf', codepage='cp866')
+    f = dbf.Table(PATH_BASE + 'warehous.dbf', codepage=CODEPAGE)
     with f.open() as ff:
         aa = ff.field_names
         row_out = ('inv', 'group_cod', 'group', 'cod', 'price_usd', 'kol', 'kol_skl', 'kol_rezerv', 'kol_sf')
@@ -22,18 +26,41 @@ def get_warehous(PATH_BASE)->list:
     return warehous
 
 
-def get_firm(PATH_BASE)-> dict:
+def get_firm(filter_id_firm: tuple = None) -> dict:
     firm = {}
-    f = dbf.Table(PATH_BASE + 'firm.dbf', codepage='cp866')
+    f = dbf.Table(PATH_BASE + 'firm.dbf', codepage=CODEPAGE)
     with f.open() as ff:
         for row in ff:
-            if row and not dbf.is_deleted(row) and not row.REC_OFF:
+            if row and not dbf.is_deleted(row) and not row.REC_OFF and (
+                    not filter_id_firm or row.FIRM in filter_id_firm):
                 firm[row.FIRM] = row.FIRM_TXT.strip()
     return firm
 
 
+def get_goods():
+    goods = []
+    f = dbf.Table(PATH_BASE + 'goods.DBF', codepage=CODEPAGE)
+    with f.open() as ff:
+        aa = ff.field_names
+        row_out = ('inv')
+        goods.append(row_out)
+        for row in ff:
+            if row and not dbf.is_deleted(row) and row.QTY:
+                row_out = (
+                    row.INV, row.GROUP + str(row.COD), row.GROUP, row.COD, row.PRICE_USD, row.KOL, row.KOL_SKL,
+                    row.KOL_REZERV,
+                    row.KOL_SF)
+                goods.append(row_out)
+
+    pass
+
+
 if __name__ == '__main__':
-    # warehous = get_warehous(PATH_BASE)
-    firm = get_firm(PATH_BASE)
-    print(firm)
+    firm = get_firm((150, 183))  # (Uninet USA, H&B)
+    assert (get_firm((150, 183)) == {150: 'Uninet USA', 183: 'H&B'}), "{150: 'Uninet USA', 183: 'H&B'}"
+
+    warehous = get_warehous()
+
+    catalogue = get_goods()
+    print(catalogue)
     pass
