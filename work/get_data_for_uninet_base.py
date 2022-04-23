6b4768cddd66c928d9d473044786a7617c46dc1c
Serg900vd <serg.vd.900@gmail.com>
2022-04-23 22:06:34 +0300

    Реализовал функцию get_goods(), возвращает словарь.

--- work/get_data_for_uninet_base.py
+++ work/get_data_for_uninet_base.py
@@ -4,13 +4,13 @@ from typing import Tuple
 
 PATH_BASE = "d:\\Kotik\\work\\2012_toner_base\\"
 CODEPAGE = 'cp1251'
-FILTR_FIRM_UNINET_HB =(150, 183)
+FILTR_FIRM_UNINET_HB = (150, 183)
 FILTER_GROUP_KM_HB = ('KM', 'HB')
 
 import dbf
 
 
-def get_warehous(filter_group:tuple=None) -> list:
+def get_warehous(filter_group: tuple = None) -> list:
     warehous = []
     f = dbf.Table(PATH_BASE + 'warehous.dbf', codepage=CODEPAGE)
     with f.open() as ff:
@@ -38,32 +38,29 @@ def get_firm(filter_id_firm: tuple = None) -> dict:
     return firm
 
 
-def get_goods():
-    goods = []
-    f = dbf.Table(PATH_BASE + 'goods.DBF', codepage=CODEPAGE)
+def get_goods(filter_group: tuple = None) -> dict:
+    goods = {}
+    f = dbf.Table(PATH_BASE + 'goods.dbf', codepage=CODEPAGE)
     with f.open() as ff:
-        aa = ff.field_names
-        row_out = ('inv')
-        goods.append(row_out)
+        field_names = ff.field_names
         for row in ff:
-            if row and not dbf.is_deleted(row) and row.QTY:
-                row_out = (
-                    row.INV, row.GROUP + str(row.COD), row.GROUP, row.COD, row.PRICE_USD, row.KOL, row.KOL_SKL,
-                    row.KOL_REZERV,
-                    row.KOL_SF)
-                goods.append(row_out)
+            if row and not dbf.is_deleted(row) and row.SHOW_PRG and (not filter_group or row.GROUP in filter_group):
+                group_cod_feilds = {}
+                for j, feild in enumerate(row):
+                    group_cod_feilds[field_names[j].lower()] = feild
+                goods[row.GROUP + str(row.COD)] = group_cod_feilds
 
-    pass
+    return goods
 
 
 if __name__ == '__main__':
-    warehous = get_warehous(FILTER_GROUP_KM_HB)
-    print(len(warehous))
+    goods = get_goods(FILTER_GROUP_KM_HB)
+    print(goods['KM750'])
 
-    firm = get_firm((150, 183))  # (Uninet USA, H&B)
-    assert (get_firm((150, 183)) == {150: 'Uninet USA', 183: 'H&B'}), "{150: 'Uninet USA', 183: 'H&B'}"
+    # warehous = get_warehous(FILTER_GROUP_KM_HB)
+    # print(len(warehous))
+    #
+    # firm = get_firm((150, 183))  # (Uninet USA, H&B)
+    # assert (get_firm((150, 183)) == {150: 'Uninet USA', 183: 'H&B'}), "{150: 'Uninet USA', 183: 'H&B'}"
 
-
-    catalogue = get_goods()
-    print(catalogue)
     pass
