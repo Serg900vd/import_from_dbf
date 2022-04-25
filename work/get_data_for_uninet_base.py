b6844416891711a61dc84b23f3bae27442aa2d70
Serg900vd <serg.vd.900@gmail.com>
2022-04-25 20:32:33 +0300

    Объединил функции get_goods и get_warehous в одну get_data_from_pass

--- work/get_data_for_uninet_base.py
+++ work/get_data_for_uninet_base.py
@@ -10,23 +10,6 @@ FILTER_GROUP_KM_HB = ('KM', 'HB')
 import dbf
 
 
-def get_warehous(filter_group: tuple = None) -> list:
-    warehous = []
-    f = dbf.Table(PATH_BASE + 'warehous.dbf', codepage=CODEPAGE)
-    with f.open() as ff:
-        aa = ff.field_names
-        row_out = ('inv', 'group_cod', 'group', 'cod', 'price_usd', 'kol', 'kol_skl', 'kol_rezerv', 'kol_sf')
-        warehous.append(row_out)
-        for row in ff:
-            if row and not dbf.is_deleted(row) and row.KOL_SKL and (not filter_group or row.GROUP in filter_group):
-                row_out = (
-                    row.INV, row.GROUP + str(row.COD), row.GROUP, row.COD, row.PRICE_USD, row.KOL, row.KOL_SKL,
-                    row.KOL_REZERV,
-                    row.KOL_SF)
-                warehous.append(row_out)
-    return warehous
-
-
 def get_firm(filter_id_firm: tuple = None) -> dict:
     """
     Возвращает, из файла firm.dbf, словарь в котором ключ поле FIRM, значение поле FIRM_TXT
@@ -47,15 +30,16 @@ def get_firm(filter_id_firm: tuple = None) -> dict:
     return firm
 
 
-def get_goods(key_field: str = 0, filter_group: tuple = None) -> dict:
+def get_data_from_pass(file_name: str, key_field: str = 0, filter_group: tuple = None) -> dict:
     """
-
+    Возвращает словарь в словаре по ключу groupcod
+    :param file_name: pass + file_name.dbf
     :param key_field: 'SHOW_PRG' Имя поля или индекс. Значение True включает строку в результат
-    :param filter_group: ('KM', 'HB') кортеж с значениями поля которые необходимо отфильтровать
-    :return:
+    :param filter_group: ('KM', 'HB') кортеж с значениями поля GROUP которые необходимо отфильтровать
+    :return: {'KM750'- gropcod : {'group': 'KM', 'cod': 750, 'groupid': 695, ...(все поля таблицы)}}
     """
-    goods = {}
-    f = dbf.Table(PATH_BASE + 'goods.dbf', codepage=CODEPAGE)
+    data = {}
+    f = dbf.Table(file_name, codepage=CODEPAGE)
     with f.open() as ff:
         field_names = ff.field_names
         for row in ff:
@@ -63,23 +47,26 @@ def get_goods(key_field: str = 0, filter_group: tuple = None) -> dict:
                 group_cod_feilds = {}
                 for j, feild in enumerate(row):
                     group_cod_feilds[field_names[j].lower()] = feild
-                goods[row.GROUP + str(row.COD)] = group_cod_feilds
-
-    return goods
+                data[row.GROUP + str(row.COD)] = group_cod_feilds
+    return data
 
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod()
 
-    goods = get_goods('SHOW_PRG', FILTER_GROUP_KM_HB)
+    goods = get_data_from_pass(PATH_BASE + 'goods.dbf', 'SHOW_PRG', FILTER_GROUP_KM_HB)
     print(goods['KM750'])
+
+    warehous = get_data_from_pass(PATH_BASE + 'warehous.dbf', 'KOL_SKL', FILTER_GROUP_KM_HB)
+    print(warehous['KM750'])
 
     # warehous = get_warehous(FILTER_GROUP_KM_HB)
     # print(len(warehous))
     #
-    firm = get_firm((151, 183))  # (Uninet USA, H&B)
-    print(firm)
+    # firm = get_firm((151, 183))  # (Uninet USA, H&B)
+    # print(firm)
     # assert (get_firm((150, 183)) == {150: 'Uninet USA', 183: 'H&B'}), "{150: 'Uninet USA', 183: 'H&B'}"
 
     pass
