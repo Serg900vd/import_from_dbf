1370959ca12a16e9afe551dda7e5f69a55dfde22
Serg900vd <serg.vd.900@gmail.com>
2022-04-25 20:51:45 +0300

    В функции get_firm добавил параметр path_base: путь к базе

--- tests/get_data_for_uninet_base_test.py
+++ tests/get_data_for_uninet_base_test.py
@@ -2,7 +2,7 @@ from unittest import TestCase, main
 import doctest
 from work import get_data_for_uninet_base
 
-PATH_BASE = "\\dbf\\"
+PATH_BASE = "dbf\\"
 CODEPAGE = 'cp1251'
 FILTR_FIRM_UNINET_HB = (150, 183)
 FILTER_GROUP_KM_HB = ('KM', 'HB')
@@ -10,11 +10,11 @@ FILTER_GROUP_KM_HB = ('KM', 'HB')
 
 class Get_firmTest(TestCase):
     def test_uninet_hb(self):
-        self.assertEqual(get_data_for_uninet_base.get_firm((150, 183)), {150: 'Uninet USA', 183: 'H&B'})
-        self.assertNotEqual(get_data_for_uninet_base.get_firm((150, 204)), {150: 'Uninet USA', 183: 'H&B'})
+        self.assertEqual(get_data_for_uninet_base.get_firm(PATH_BASE, (150, 183)), {150: 'Uninet USA', 183: 'H&B'})
+        self.assertNotEqual(get_data_for_uninet_base.get_firm(PATH_BASE, (150, 204)), {150: 'Uninet USA', 183: 'H&B'})
 
     def test_olansi(self):
-        self.assertEqual(get_data_for_uninet_base.get_firm((204,)), {204: 'Olansi'})
+        self.assertEqual(get_data_for_uninet_base.get_firm(PATH_BASE, (204,)), {204: 'Olansi'})
 
 
 def load_tests(loader, tests, ignore):
--- work/get_data_for_uninet_base.py
+++ work/get_data_for_uninet_base.py
@@ -1,7 +1,5 @@
 # Обновление данных для UNINET base.xls
 #
-from typing import Tuple
-
 PATH_BASE = "d:\\Kotik\\work\\2012_toner_base\\"
 CODEPAGE = 'cp1251'
 FILTR_FIRM_UNINET_HB = (150, 183)
@@ -10,18 +8,19 @@ FILTER_GROUP_KM_HB = ('KM', 'HB')
 import dbf
 
 
-def get_firm(filter_id_firm: tuple = None) -> dict:
+def get_firm(path_base: str, filter_id_firm: tuple = None) -> dict:
     """
     Возвращает, из файла firm.dbf, словарь в котором ключ поле FIRM, значение поле FIRM_TXT
-    :param filter_id_firm:
+    :param path_base: путь к базе
+    :param filter_id_firm: (150, 183) коды FIRM которые необходимо отфильтровать
     :return: dict
-    >>> get_firm((150, 183))
+    >>> get_firm(PATH_BASE, (150, 183))
     {150: 'Uninet USA', 183: 'H&B'}
-    >>> get_firm((150, ))
+    >>> get_firm(PATH_BASE, (150, ))
     {150: 'Uninet USA'}
     """
     firm = {}
-    f = dbf.Table(PATH_BASE + 'firm.dbf', codepage=CODEPAGE)
+    f = dbf.Table(path_base + 'firm.dbf', codepage=CODEPAGE)
     with f.open() as ff:
         for row in ff:
             if row and not dbf.is_deleted(row) and not row.REC_OFF and (
