67b4a7f3acb7e53f7058c39e572ea7424b27ec0e
Serg900vd <serg.vd.900@gmail.com>
2022-05-17 21:46:59 +0300

    class BasePassDBF Доработал методы get_warehous_grcod_filter() get_warehous_grcod_sum()

--- uninet/get_data_uninet.py
+++ uninet/get_data_uninet.py
@@ -3,6 +3,7 @@
 import csv
 import os
 from sys import argv
+from typing import List
 
 import dbf
 
@@ -100,15 +101,26 @@ class BasePassDBF:
         self.set_firm()
         print('firm loaded')
 
-    def get_warehous_grcod_filter(self, group_cod):
+    def get_warehous_grcod_filter(self, group_cod: str) -> List[dict]:
+        """
+        Получаем список всех строк (приходы) для заданного продукта (уникальный ключ 'group_cod') из таблицы warehous
+        :param group_cod:
+        :return:
+        """
         return [row for row in self.warehous.values() if row['group'] + str(row['cod']) == group_cod]
 
-    def get_warehous_grcod_sum(self, group_cod):
+    def get_warehous_grcod_sum(self, group_cod: str) -> dict:
+        """
+        Суммируем приходы для заданного продукта
+        :param group_cod:
+        :return:
+        """
         _group_cod_list = self.get_warehous_grcod_filter(group_cod)
 
-        _parameters = ['kol', 'kol_otkaz', 'kol_rezerv', 'kol_sale', 'kol_sf', 'kol_skl', 'old_sale']
         _group_cod_sum = {'group_cod': group_cod, 'kol': 0, 'kol_otkaz': 0, 'kol_rezerv': 0, 'kol_sale': 0,
-                         'kol_sf': 0, 'kol_skl': 0, 'old_sale': 0}
+                          'kol_sf': 0, 'kol_skl': 0, 'old_sale': 0}
+        _parameters = list(_group_cod_sum.keys())
+        _parameters.remove('group_cod')
 
         for row in _group_cod_list:
             for param in _parameters:
