bac9ac9a9e0c6869369752a05c980c6f03f4ccad
Serg900vd <serg.vd.900@gmail.com>
2022-07-31 19:53:55 +0300

    Refactoring test_prom.py TestWriteProductsPromOn with read_yaml and unittest.mock.

--- tests/test_prom.py
+++ tests/test_prom.py
@@ -16,41 +16,24 @@ PATH_TESTS_DATA = PATH_TESTS / 'prom_test_data'
 
 
 @skipIf(SKIP_REAL, 'Skipping tests that hit the real API server.')
+class TestWriteProductsPromOnRealAPI(TestCase):
+    def test_write_products_prom(self):
+        products_changed_list = read_yaml('test_write_products_prom.yaml')
+        self.assertTrue(write_products_prom(products_changed_list))
+
+    def test_write_products_prom_err(self):
+        err_products_changed_list = read_yaml('test_write_products_prom_err.yaml')
+        self.assertRaises(ValueError, write_products_prom, err_products_changed_list)
+
+
 class TestWriteProductsProm(TestCase):
     def test_write_products_prom(self):
-        products_changed_list = [{'external_id': 'test_HB770',
-                                  'id': 1616486427,
-                                  'presence': 'not_available',
-                                  'price': 8.5,
-                                  'prices': [{'minimum_order_quantity': 5.0, 'price': 8.0}],
-                                  'status': 'not_on_display'},
-                                 {'external_id': 'OL6',
-                                  'id': 628473798,
-                                  'presence': 'not_available',
-                                  'price': 16.73,
-                                  'prices': [{'minimum_order_quantity': 2.0, 'price': 15.06}],
-                                  'status': 'on_display'},
-                                 ]
+        products_changed_list = read_yaml('test_write_products_prom.yaml')
+
         self.assertTrue(write_products_prom(products_changed_list))
 
     def test_write_products_prom_empty(self):
         self.assertFalse(write_products_prom([]))
-
-    def test_write_products_prom_err(self):
-        err_products_changed_list = [{'external_id': 'test_HB770',
-                                      'id': 1616486427,
-                                      'presence': 'ERRRORnot_available',
-                                      'price': 8.5,
-                                      'prices': [{'minimum_order_quantity': 5.0, 'price': 8.0}],
-                                      'status': 'not_on_display'},
-                                     {'external_id': 'OL6',
-                                      'id': 628473798,
-                                      'presence': 'not_available',
-                                      'price': 16.73,
-                                      'prices': [{'minimum_order_quantity': 2.0, 'price': 15.06}],
-                                      'status': 'on_display'},
-                                     ]
-        self.assertRaises(ValueError, write_products_prom, err_products_changed_list)
 
 
 def read_yaml(file: str) -> Union[list, dict]:
@@ -93,7 +76,7 @@ class TestReadProductsProm(TestCase):
         last_id = 637949599
         products_prom_test = read_yaml('test_read_products_prom_637949599.yaml')
         products_prom = read_products_prom(last_id)
-        # with open(path_data, 'w') as f:
+        # with open(PATH_TESTS_DATA / 'test_read_products_prom_637949599.yaml', 'w') as f:
         #     yaml.dump(products_prom, f, default_flow_style=False)
 
         self.assertListEqual(products_prom_test, products_prom)
